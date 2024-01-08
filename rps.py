7e2f7962946e66c0f924f4ba5ac4135b20d55324
Abhya Singh <abhya.singh@wfe.com>
2024-01-08 14:56:52 +0530

    game

---
+++ rps.py
@@ -0,0 +1,33 @@
+import random 
+while True : 
+    choices = ['rock' , 'paper' , 'scissor']
+    computer = random.choice(choices)
+    player = None
+    while player not in choices  :
+        player = input('choose a option rock , paper , scissor  :  ').lower()
+        print ( 'player choose : '+ player)
+        print('computer choose : '+ computer)
+        if computer == player : 
+            print("it's a tie !")
+        elif player == 'scissor' :
+            if computer == 'rock':
+                print("player loose !") 
+            else:
+                print("player won!")
+        elif player == 'paper' :
+            if computer == 'scissor':
+                print("player loose !") 
+            else:
+                print("player won!")
+        elif player == 'rock' :
+            if computer == 'paper':
+                print("player loose !") 
+            else:
+                print("player won!")
+    
+    playagain = input("do you wanna play again? : yes/no ").lower()
+    if playagain != 'yes' :
+        break    
+print('bye, have a great day !')
+
+
